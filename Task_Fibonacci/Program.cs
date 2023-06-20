d7fd035d5663b2f10d27647606140937cb17d2c2
Sadak <s.sadak02@gmail.com>
2023-06-20 15:27:04 +0600

    Ряд Фибоначчи через рекурсию

---
+++ Task_Fibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿// Фибоначчи в рекурсии
+
+int Func (int num)
+{
+    if (num == 0) return 0;
+    if (num ==1) return 1;
+    return Func (num-1) + Func (num-2);
+}
+
+string Res(int num)
+{
+    string str = "";
+    for (int i = 0; i < num; i++)
+    {
+        str+=$"{Func(i)} ";
+    }
+    return str;
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Func(num));
+System.Console.WriteLine(Res(num));
+
+// int Func(int num)
+// {
+//     if (num == 0 || num == 1) return num;
+//     return Func(num - 1) + Func(num - 2);
+// }
+
+// string Res(int num)
+// {
+//     string str = "";
+//     for (int i = 0; i < num; i++)
+//     {
+//         str += $"{Func(i)} ";
+//     }
+//     return str;
+// }
+
+// System.Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(Res(num));
