2ac6f81c90c74858ff13ccce9d24aa44c85c525b
Sadak <s.sadak02@gmail.com>
2023-06-20 15:21:50 +0600

    Все натуральные числа от 1 до N

---
+++ Task063/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+string NumTillN (int num)
+{
+    if (num==1) return "1";
+    return NumTillN(num-1) + $", {num}";
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(NumTillN(num));
+
+// string GetNumbers(int num)
+// {
+//     string str = "";
+//      while (true)
+//     {
+//         str = Convert.ToString(num) + " " + str;
+//         num--;
+//         if (num==0)
+//             break;
+//     }
+//     return str;
+// }
+
+// string GetNumbersRec(int num)
+// {
+//     if (num == 0)  return "";
+//     return GetNumbersRec(num - 1) + num + " " ;    
+// }
+
+// Console.Clear();
+
+// System.Console.WriteLine("Введите целое число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(GetNumbersRec(num));
