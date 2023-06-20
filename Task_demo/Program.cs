b7c99eff9c4acd1a64672c8f3dd1cf1be04f618c
Sadak <s.sadak02@gmail.com>
2023-06-20 15:18:47 +0600

    Сумма целых числе от 1 до N

---
+++ Task_demo/Program.cs
@@ -0,0 +1,29 @@
+﻿// посчитаем сумму всех целых чисел от 1 до N, где N вводит пользователь
+int Summa(int num)
+{
+    int sum = 0;
+    while (true)
+    {
+        sum+=num;
+        num--;
+        if (num==0)
+            break;
+    }
+    return sum;
+}
+
+int SummaRec(int num)
+{
+    if (num==0)  return 0;
+    return num + SummaRec(num-1);
+}
+
+//SummaRec(4)
+//4 + (3 + (2 + (1 + 0)))
+
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма всех целых чисел от 1 до {num} равна {Summa(num)}");
+System.Console.WriteLine($"а если рекурсией то {SummaRec(num)}");
